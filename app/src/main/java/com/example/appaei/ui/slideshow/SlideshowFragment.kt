5ff93c56b88e8bb15466a07c38a6ec1ba5c3c747
chasqui96 <chasqui270596@gmail.com>
2024-10-18 23:38:13 -0300

    nuevos cambios

--- app/src/main/java/com/example/appaei/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/appaei/ui/slideshow/SlideshowFragment.kt
@@ -1,20 +1,18 @@
 package com.example.appaei.ui.slideshow
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.appaei.databinding.FragmentSlideshowBinding
 
-class SlideshowFragment : Fragment() {
+class SlideShowFragment : Fragment() {
 
     private var _binding: FragmentSlideshowBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // Esta propiedad solo es válida entre onCreateView y onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,16 +20,15 @@ class SlideshowFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
 
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        // Configura el botón para registrar un log al hacer clic
+        binding.irretroalimentacion.setOnClickListener {
+            Log.d("SlideShowFragment", "El botón fue presionado.")
         }
+
         return root
     }
 
